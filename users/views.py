8d1f8419b9f6a1e086137e3268a4ae99dacbc609
Inna Misovetc <inna.misovets@skoltech.ru>
2021-11-24 16:45:05 +0300

    - changed permission for UserListView to IsAuthenticated - added UserDetailView

--- users/views.py
+++ users/views.py
@@ -1,17 +1,30 @@
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from comments.models import Comment
+from comments.serializers import CommentSerializer
+from secure_t_api.permissions import IsActiveUserOrReadOnly
 from .models import User
 from .serializers import UserSerializer
-from rest_framework.generics import ListAPIView, CreateAPIView
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 
 class UserListView(ListAPIView):
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAuthenticated]
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class UserCreateView(CreateAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserDetailView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsActiveUserOrReadOnly]
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'username'

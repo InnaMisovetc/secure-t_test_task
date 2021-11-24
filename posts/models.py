fd9b6dc873dc72d8fc42f34d49c2cbb06c572e51
Inna Misovetc <inna.misovets@skoltech.ru>
2021-11-24 16:56:56 +0300

    Changed id field of Post model to uuid

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,13 @@
-from django.db import models
+import uuid
+
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
 
 class Post(models.Model):
+    id = models.CharField(max_length=100, unique=True, blank=True, default=uuid.uuid4, primary_key=True, name='post_id')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     content = models.TextField()

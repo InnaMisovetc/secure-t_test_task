9fd8bdc2694d5ad0162f9beda385077401c228b7
Inna Misovetc <inna.misovets@skoltech.ru>
2021-11-24 18:05:45 +0300

    fixed <post_id>/comments/ url

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 
 from .views import PostListCreateView, PostDetailView
-from comments.views import CommentCreateView, CommentDetailView
 
 urlpatterns = [
     path('', PostListCreateView.as_view()),
     path('<post_id>/', PostDetailView.as_view()),
-    path('comments/', include('comments.urls')),
+    path('<post_id>/comments/', include('comments.urls')),
 ]

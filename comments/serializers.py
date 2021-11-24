ab6ba15881cfddbdbba92e0a7c660ecd938f9ef9
Inna Misovetc <inna.misovets@skoltech.ru>
2021-11-24 16:55:59 +0300

    Renamed id field of CommentSerializer to comment_id

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,6 +1,7 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Comment
 from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+
+from .models import Comment
 
 
 class RecursiveSerializer(serializers.Serializer):
@@ -12,17 +13,16 @@ class RecursiveSerializer(serializers.Serializer):
 class CommentSerializer(ModelSerializer):
     replies = RecursiveSerializer(many=True, read_only=True)
     author = serializers.ReadOnlyField(source='author.username')
+    post = serializers.ReadOnlyField(source='post.post_id')
 
     class Meta:
         model = Comment
-        fields = ['id', 'author', 'content', 'post', 'parent', 'updated_at', 'replies']
-        extra_kwargs = {'id': {'read_only': True}}
+        fields = ['comment_id', 'author', 'content', 'post', 'parent', 'updated_at', 'replies']
+        extra_kwargs = {'comment_id': {'read_only': True}}
 
-    def validate(self, data):
-        if data['parent']:
-            if data['parent'].post != data['post']:
-                raise serializers.ValidationError('The parent comment you want to comment has another post number')
-        return data
-
-
-
+    def update(self, instance, validated_data):
+        if validated_data['post'] != instance.post:
+            raise serializers.ValidationError('You must not change post id.')
+        if validated_data['parent'] != instance.parent:
+            raise serializers.ValidationError('You must not change parent comment id.')
+        return super().update(instance, validated_data)

8142521f88040ff3b06cf2578a6265820208cf9a
Inna Misovetc <inna.misovets@skoltech.ru>
2021-11-24 16:50:32 +0300

    added comments/ endpoint

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+
 from .views import PostListCreateView, PostDetailView
+from comments.views import CommentCreateView, CommentDetailView
 
 urlpatterns = [
     path('', PostListCreateView.as_view()),
-    path('<id>/', PostDetailView.as_view())
+    path('<post_id>/', PostDetailView.as_view()),
+    path('comments/', include('comments.urls')),
 ]
